8f101854887526288b4f63c3b4b40abf7241d571
Steel <https://github.com/Steelmen87/>
2022-07-25 15:15:19 +0300

    HW3 done+

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -23,10 +23,6 @@ function HW3() {
 
             {/*should work (должно работать)*/}
             <GreetingContainer users={users} addUserCallback={addUserCallback}/>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeGreeting/>*/}
             <hr/>
         </div>
     )
