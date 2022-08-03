8f7a9ac240d43e4c4c6ff90420dfd5ca8bbe858d
Steel <Steelmen87@bk.ru>
2022-08-03 16:13:24 +0300

    HW5 Done

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import style from './pages/style.module.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <input type="checkbox" id="hmt" className={style["hidden-menu-ticker"]}/>
+            <label className={style["btn-menu"]} htmlFor="hmt">
+                <span className={style.first}/>
+                <span className={style.second}/>
+                <span className={style.third}/>
+            </label>
+            <ul className={style["hidden-menu"]}>
+                <li><NavLink to={'/'}>Pre Junior</NavLink></li>
+                <li><NavLink to={PATH.JUNIOR}>Junior</NavLink></li>
+                <li><NavLink to={PATH.JUNIORPlUS}>Junior Plus</NavLink></li>
+            </ul>
         </div>
     )
 }
