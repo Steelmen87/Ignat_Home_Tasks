f0adf191ecb3a74e4e4eb547181323feb5c1253a
Steel <Steelmen87@bk.ru>
2022-09-08 14:59:36 +0300

    HW11

--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
-import s from './SuperRange.module.css'
+import React, {DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import InputRange from "react-input-range";
+import style from "../c8-SuperDoubleRange/style.module.css";
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -7,35 +8,37 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeRange?: (value: number) => void
+    setState: (value: any) => void
+    state: any
+    step: number
 };
 
 const SuperRange: React.FC<SuperRangePropsType> = (
     {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
-        onChange, onChangeRange,
+        type,
+        onChange,
         className,
-
+        state, setState, step,
         ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e) // сохраняем старую функциональность
 
-        onChangeRange && onChangeRange(+e.currentTarget.value)
+    const setValueHandler = (value: any) => {
+        if (value + step <= state.value5.max) {
+            setState({value5: {min: value, max: state.value5.max}})
+        }
     }
-
-    const finalRangeClassName = `${s.range} ${className ? className : ''}`
-
     return (
         <>
-            <input
-                type={'range'}
-                onChange={onChangeCallback}
-                className={finalRangeClassName}
-
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
+            <form className={style.form}>
+                <InputRange
+                    step={step}
+                    maxValue={50}
+                    minValue={0}
+                    value={state.value5.min}
+                    onChange={setValueHandler}
+                />
+            </form>
         </>
     )
 }
