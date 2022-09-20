f451df0a51763214dde8d789978ae25751914bd1
Steel <Steelmen87@bk.ru>
2022-09-20 13:03:11 +0300

    HW13+

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -5,21 +5,29 @@ import {RequestsAPI} from "./api/api";
 
 const Request = () => {
     const [status, setStatus] = useState(false)
+    const [response, setResponse] = useState('')
     const statusCheckbox = (status: boolean) => {
         setStatus(status)
     }
     const onClickRequest = () => {
         RequestsAPI.ignatiusServer(status)
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res)
+                setResponse(res.data.errorText)
+            })
             .catch((error) => {
                 console.log({...error});
                 console.log(error.response ? error.response.data.errorText : error.message);
+                if(error.response){
+                    setResponse(error.response.data.errorText)
+                }
             })
 
     }
     return (
         <div style={{paddingLeft: '20px'}}>
-            <div>HW13</div>
+            <span>HW13</span><span> - {response}</span>
+            <br/>
             <SuperButton onClick={onClickRequest}>Request</SuperButton>
             <br/>
             <SuperCheckbox onChangeChecked={statusCheckbox}>click me</SuperCheckbox>
